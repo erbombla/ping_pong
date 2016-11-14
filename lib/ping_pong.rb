86bae7afdcfc187cf0131408d341642a99828e0e
Eric Bomblatus and Margie Nevarez <margie@>
2016-11-14 11:40:27 -0800

    add last spec needed and pass all tests

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -2,10 +2,12 @@ class Fixnum
   define_method(:ping_pong) do
     count = []
     (1..self).each() do |time|
-      if time.%(3) == 0
-        count.push("ping")
+      if time.%(15) == 0
+        count.push("ping-pong")
       elsif time.%(5) == 0
         count.push("pong")
+      elsif time.%(3) == 0
+        count.push("ping")
       else
         count.push(time)
       end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -6,11 +6,16 @@ describe('Fixnum#ping_pong') do
     expect((2).ping_pong).to(eq([1,2]))
   end
 
-  it('Determine if each number is divisible by 3') do
-    expect((3).ping_pong).to(eq([1,2,"ping"]))
+  it('Determine if each number is divisible by 15') do
+    expect((15).ping_pong).to(eq([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"ping-pong"]))
   end
 
   it('Determine if each number is divisible by 5') do
     expect((10).ping_pong).to(eq([1,2,"ping",4,"pong","ping",7,8,"ping","pong"]))
   end
+
+  it('Determine if each number is divisible by 3') do
+    expect((3).ping_pong).to(eq([1,2,"ping"]))
+  end
+
 end
